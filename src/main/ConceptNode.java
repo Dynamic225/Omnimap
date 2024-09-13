9f19368785bfe4db8bc57eabfd2c008ab48b5a5d
Dynamic <anthonywitherspoon225@gmail.com>
2024-09-13 00:55:27 -0500

    ConceptNode Defined well

--- src/main/ConceptNode.java
+++ src/main/ConceptNode.java
@@ -1,8 +1,20 @@
 package main;
 
-import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
-public class ConceptNode {
+public class ConceptNode implements Comparable<ConceptNode> {
     String conceptText;
     ConceptNode parent = null;
+    SortedSet<ConceptNode> related;
+
+    public ConceptNode(String conceptText, SortedSet<ConceptNode> related) {
+        this.conceptText = conceptText;
+        this.related = new TreeSet<ConceptNode>();
+    }
+
+    @Override
+    public int compareTo(ConceptNode otherNode) {
+        return this.conceptText.compareTo(otherNode.conceptText);
+    }
 }
